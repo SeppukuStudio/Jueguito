36b4a113b6f9fe16512e00cd37ec91fdb22303b8
Raúl Serrano Gómez <raulserranodev@gmail.com>
2019-07-13 19:54:51 +0200

    13.07 Creado Proyecto de Unity

---
+++ Jueguito/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Controla el movimiento del personaje
+/// </summary>
+public class PlayerMovement : MonoBehaviour
+{
+    private Vector2 inputMovement;
+
+    private void Update()
+    {
+        //Touch myTouch = Input.GetTouch(0);
+
+        //Touch[] myTouches = Input.touches;
+        //for (int i = 0; i < Input.touchCount; i++)
+        //{
+        //    //Do something with the touches
+        //}
+
+        //TEST
+        transform.Rotate(Vector3.forward, 5);
+    }
+}
