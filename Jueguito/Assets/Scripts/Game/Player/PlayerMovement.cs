1a4e52a4861caac655b088b739c1865c4907cfa0
Raúl Serrano Gómez <raulserranodev@gmail.com>
2019-07-15 20:21:51 +0200

    15.07. Deadzone al movimiento y orientación fijada

--- Jueguito/Assets/Scripts/Game/Player/PlayerMovement.cs
+++ Jueguito/Assets/Scripts/Game/Player/PlayerMovement.cs
@@ -8,7 +8,15 @@ namespace Jueguito
     public class PlayerMovement : MonoBehaviour
     {
         [Header("Attributes")]
+        /// <summary>
+        /// Velocidad de movimiento del personaje
+        /// </summary>
         public float Velocity;
+
+        /// <summary>
+        /// Distancia que debe haber entre dedo y pj para que se realice el movimiento
+        /// </summary>
+        public float Deadzone;
 
         //Components
         private Rigidbody2D rb;
@@ -39,9 +47,16 @@ namespace Jueguito
                 Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
                 touchPosition.z = 0f; //No queremos mover en z el personaje.
 
-                //Movemos el personaje
-                inputMovement = new Vector2(touchPosition.x - transform.position.x, touchPosition.y - transform.position.y);
-                inputMovement.Normalize();
+                Vector2 distance = touchPosition - transform.position;
+
+                if (distance.magnitude <= Deadzone)
+                    inputMovement = Vector2.zero;
+                else
+                {
+                    //Movemos el personaje
+                    inputMovement = new Vector2(distance.x, distance.y);
+                    inputMovement.Normalize();
+                }
 
                 //Para el estado actual del touch(Began, Ended, Moved, Stationary, Canceled)
                 // Debug.Log(touch.phase);
@@ -50,24 +65,23 @@ namespace Jueguito
                 inputMovement = Vector2.zero;
 
             //Recorrer todos los dedos y dibuja lineas a cada dedo
-            Touch[] touches = Input.touches;
-            for (int i = 0; i < Input.touchCount; i++)
-            {
-                //Dedo concreto
-                Touch touch = touches[i];
+            //Touch[] touches = Input.touches;
+            //for (int i = 0; i < Input.touchCount; i++)
+            //{
+            //    //Dedo concreto
+            //    Touch touch = touches[i];
 
-                //Obtenemos la posición en el mundo. Touch.position nos devuelve la posición del dedo en pixeles
-                Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
-                touchPosition.z = 0f; //No queremos mover en z el personaje.
+            //    //Obtenemos la posición en el mundo. Touch.position nos devuelve la posición del dedo en pixeles
+            //    Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+            //    touchPosition.z = 0f; //No queremos mover en z el personaje.
 
-                Debug.DrawLine(Vector3.zero, touchPosition, Color.red);
-            }
+            //    Debug.DrawLine(Vector3.zero, touchPosition, Color.red);
+            //}
         }
 
         private void FixedUpdate()
         {
             //Rigidbody: Mass, Drag y Gravity para modificar las propiedades del jugador
-
             Vector3 movement = inputMovement * Velocity;
             rb.velocity = movement;
         }
