c945f871e4c513fa67b9b9add156ee44fa7f26eb
Raúl Serrano Gómez <raulserranodev@gmail.com>
2019-07-16 19:08:06 +0200

    16.07. Lo que he hecho de enemy. No va

---
+++ Jueguito/Assets/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    //Own references
+    private NavMeshAgent navMeshAgent;
+
+    /// <summary>
+    /// Obtiene referencias
+    /// </summary>
+    private void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    /// <summary>
+    /// Persigue al jugador
+    /// </summary>
+    private void Update()
+    {
+        navMeshAgent.SetDestination(transform.position - new Vector3(0,1,0));
+
+        //Comprueba si ha llegado al destino
+        if (navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)
+        {
+            //Do something...
+        }
+    }
+}
